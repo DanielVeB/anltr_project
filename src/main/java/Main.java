a5f546616ce43ee623ceb0d7d325eba044fd3d47
DanielVeB <danielkurosz@gmail.com>
2020-05-21 21:51:43 +0200

    Update pom, init Main and Translator , Move antlr files to main folder

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,30 @@
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+
 public class Main {
+
+
+    public static void main(String[] args) {
+
+        String file = "src/main/resources/examples/example1.txt";
+
+        Translator translator = new Translator();
+
+        CharStream stream = null;
+
+        try {
+            stream = CharStreams.fromFileName(file);
+        }catch (Exception e){
+            System.out.println("UPS");
+        }
+        Fortran77Lexer lexer = new Fortran77Lexer(stream);
+
+        CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+
+        Fortran77Parser parser = new Fortran77Parser(tokenStream);
+        Fortran77Parser.ProgramContext context= parser.program();
+        translator.visit(context);
+
+    }
 }
--- src/main/java/Translator.java
+++ src/main/java/Translator.java
@@ -1,2 +1,8 @@
-public class Translator {
+public class Translator extends Fortran77ParserBaseVisitor<String>{
+
+    @Override
+    public String visitProgram(Fortran77Parser.ProgramContext ctx) {
+        return ctx.getText();
+    }
+
 }
