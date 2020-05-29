243b472fad471eba1e88585dfe4210454f5a9cd2
MMBorkowski <34061346+MMBorkowski@users.noreply.github.com>
2020-05-30 00:34:26 +0200

    function header + variable class

--- src/main/resources/clang/basic_1/hello.c
+++ src/main/resources/clang/basic_1/hello.c
@@ -1,5 +1,8 @@
-#include <stdio.h>
-int main() {
-   printf("Hello, World!");
-   return 0;
+float func(float a, float b){
+return a + b + 2.0;
+}
+
+float main() {
+   float b = func(3.2, 4.1);
+   return b;
 }
